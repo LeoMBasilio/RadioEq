8789aa59517bbfa822823fe465c66e5cadc3f269
Leonardo Basilio <ew.basilio@gmail.com>
2023-07-30 10:16:03 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,186 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import OneHotEncoder
+#from pandas.core.dtypes.cast import maybe_upcast
+from sklearn.metrics.pairwise import euclidean_distances
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
+
+import matplotlib.pyplot as plt
+from skimage import io
+
+
+
+dados = pd.read_csv('https://raw.githubusercontent.com/sthemonica/music-clustering/main/Dados/Dados_totais.csv')
+dados_generos = pd.read_csv('https://raw.githubusercontent.com/sthemonica/music-clustering/main/Dados/data_by_genres.csv')
+dados_anos = pd.read_csv('https://raw.githubusercontent.com/sthemonica/music-clustering/main/Dados/data_by_year.csv')
+
+dados["year"].unique()
+dados = dados.drop(["explicit", "key", "mode"], axis=1)
+dados.isna().sum()
+
+dados_generos = dados_generos.drop(["key","mode"], axis=1)
+dados_generos.isnull().sum()
+
+dados_anos["year"].unique()
+dados_anos = dados_anos[dados_anos["year"] >= 1921]
+dados_anos["year"].unique()
+
+dados_anos = dados_anos.drop(["key","mode"], axis=1)
+dados_anos.reset_index()
+
+# #--Graficos--#
+# fig = px.line(dados_anos, x="year", y="loudness", markers= True, title='Variação do loudness conforme os anos')
+# fig.show()
+
+# fig = go.Figure()
+
+# fig.add_trace(go.Scatter(x=dados_anos['year'], y=dados_anos['acousticness'],
+#                     name='Acousticness'))
+# fig.add_trace(go.Scatter(x=dados_anos['year'], y=dados_anos['valence'],
+#                     name='Valence'))
+# fig.add_trace(go.Scatter(x=dados_anos['year'], y=dados_anos['danceability'],
+#                     name='Danceability'))
+# fig.add_trace(go.Scatter(x=dados_anos['year'], y=dados_anos['energy'],
+#                     name='Energy'))
+# fig.add_trace(go.Scatter(x=dados_anos['year'], y=dados_anos['instrumentalness'],
+#                     name='Instrumentalness'))
+# fig.add_trace(go.Scatter(x=dados_anos['year'], y=dados_anos['liveness'],
+#                     name='Liveness'))
+# fig.add_trace(go.Scatter(x=dados_anos['year'], y=dados_anos['speechiness'],
+#                     name='Speechiness'))
+
+# fig.show()
+
+# fig = px.imshow(dados.corr(), text_auto=True)
+# fig.show()
+
+dados_generos1 = dados_generos.drop('genres', axis=1)
+
+#--cluster por genero--#
+
+SEED = 1224
+np.random.seed(SEED)
+
+pca_pipeline = Pipeline([('scaler', StandardScaler()), ('PCA', PCA(n_components=2, random_state=SEED))])
+genre_embedding_pca = pca_pipeline.fit_transform(dados_generos1)
+projection = pd.DataFrame(columns=['x', 'y'], data=genre_embedding_pca)
+
+kmeans_pca = KMeans(n_clusters=5, verbose=0, random_state=SEED)
+kmeans_pca.fit(projection)
+
+dados_generos['cluster_pca'] = kmeans_pca.predict(projection)
+projection['cluster_pca'] = kmeans_pca.predict(projection)
+projection['generos'] = dados_generos['genres']
+
+#--cluster por musica--#
+
+ohe = OneHotEncoder(dtype=int)
+colunas_ohe = ohe.fit_transform(dados[['artists']]).toarray()
+dados2 = dados.drop('artists', axis=1)
+
+dados_musicas_dummies = pd.concat([dados2, pd.DataFrame(colunas_ohe, columns=ohe.get_feature_names_out(['artists']))], axis=1)
+
+pca_pipeline = Pipeline([('scaler', StandardScaler()), ('PCA', PCA(n_components=0.7, random_state=SEED))])
+
+
+music_embedding_pca = pca_pipeline.fit_transform(dados_musicas_dummies.drop(['id','name','artists_song'], axis=1))
+projection_m = pd.DataFrame(data=music_embedding_pca)
+
+kmeans_pca_pipeline = KMeans(n_clusters=50, verbose=False, random_state=SEED)
+
+kmeans_pca_pipeline.fit(projection_m)
+
+dados['cluster_pca'] = kmeans_pca_pipeline.predict(projection_m)
+projection_m['cluster_pca'] = kmeans_pca_pipeline.predict(projection_m)
+
+projection_m['artist'] = dados['artists']
+projection_m['song'] = dados['artists_song']
+
+#--recomentação--#
+
+nome_musica = 'Ed Sheeran - Shape of You'
+
+cluster = list(projection_m[projection_m['song']== nome_musica]['cluster_pca'])[0]
+musicas_recomendadas = projection_m[projection_m['cluster_pca']== cluster][[0, 1, 'song']]
+x_musica = list(projection_m[projection_m['song']== nome_musica][0])[0]
+y_musica = list(projection_m[projection_m['song']== nome_musica][1])[0]
+
+#distâncias euclidianas
+distancias = euclidean_distances(musicas_recomendadas[[0, 1]], [[x_musica, y_musica]])
+musicas_recomendadas['id'] = dados['id']
+musicas_recomendadas['distancias']= distancias
+recomendada = musicas_recomendadas.sort_values('distancias').head(10)
+
+scope = "user-library-read playlist-modify-private"
+OAuth = SpotifyOAuth(
+        scope=scope,         
+        redirect_uri='http://localhost:5000/callback',
+        client_id = '7695b998110c4978aa5bf63cbb4c2ab4',
+        client_secret = '55c853b17e82499b9bdf1e44af210b13')
+
+client_credentials_manager = SpotifyClientCredentials(client_id = '7695b998110c4978aa5bf63cbb4c2ab4',client_secret = '55c853b17e82499b9bdf1e44af210b13')
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+scope = "user-library-read playlist-modify-private"
+OAuth = SpotifyOAuth(
+        scope=scope,         
+        redirect_uri='http://localhost:5000/callback',
+        client_id = '7695b998110c4978aa5bf63cbb4c2ab4',
+        client_secret = '55c853b17e82499b9bdf1e44af210b13')
+
+client_credentials_manager = SpotifyClientCredentials(client_id = '7695b998110c4978aa5bf63cbb4c2ab4',client_secret = '55c853b17e82499b9bdf1e44af210b13')
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+def recomendador(nome_musica):
+
+## Calculando as distâncias
+  cluster = list(projection_m[projection_m['song']== nome_musica]['cluster_pca'])[0]
+  musicas_recomendadas = projection_m[projection_m['cluster_pca']== cluster][[0, 1, 'song']]
+  x_musica = list(projection_m[projection_m['song']== nome_musica][0])[0]
+  y_musica = list(projection_m[projection_m['song']== nome_musica][1])[0]
+  distancias = euclidean_distances(musicas_recomendadas[[0, 1]], [[x_musica, y_musica]])
+  musicas_recomendadas['id'] = dados['id']
+  musicas_recomendadas['distancias'] = distancias
+  recomendada = musicas_recomendadas.sort_values('distancias').head(10)
+
+  # ## Acessando os dados de cada música com a biblioteca Spotipy (nome e imagem)
+  playlist_id = recomendada['id']
+
+  url = []
+  name = []
+  for i in playlist_id:
+      track = sp.track(i)
+      url.append(track["album"]["images"][1]["url"])
+      name.append(track["name"])
+
+# ## Plotando as figuras
+  plt.figure(figsize=(15,10))
+  columns = 5
+  for i, u in enumerate(url):
+      ax = plt.subplot(len(url) // columns + 1, columns, i + 1)
+      image = io.imread(u)
+      plt.imshow(image)
+      ax.get_yaxis().set_visible(False)
+      plt.xticks(color = 'w', fontsize = 0.1)
+      plt.yticks(color = 'w', fontsize = 0.1)
+      plt.xlabel(name[i], fontsize = 10)
+      plt.tight_layout(h_pad=0.7, w_pad=0)
+      plt.subplots_adjust(wspace=None, hspace=None)
+      plt.grid(visible=None)
+      plt.tick_params(bottom = False)
+  plt.show()
+
+print("digite a música que você deseja recomendar")
+print("exemplo: Ed Sheeran - Shape of You")
+nome_musica = input()
+recomendador(nome_musica)
